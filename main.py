56ee29fe3eef5c42665f97799225d7bbee48a42d
Andrzej Pawcenis <a.pawcenis@gmail.com>
2019-12-01 19:11:10 +0100

    add basic superblock fields

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ with open("fs.img", "rb") as file:
     _ = file.read(1024)
     superblock = file.read(1024)
 
-superblock_struct = [
-    ['total_inodes', 0, 3, 4],
-    ['total_blocks', 4, 7, 4],
-    ['blocks_reserved_for_superuser', 8, 11, 4],
-    ['unnalocated_blocks', 12, 15, 4],
-    ['unnalocated_inodes', 16, 19, 4],
-    ['block_containing_superblock', 20, 23, 4],
-    ['block_size', 24, 27, 4]
-]
-
 def read_superblock(superblock, start, end):
     return superblock[start:end]
 
