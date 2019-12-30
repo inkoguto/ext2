a350f55df1693c57ef4a0cecace3a452d0fb3fd2
Andrzej Pawcenis <a.pawcenis@gmail.com>
2019-12-30 07:33:53 +0100

    refactor directory

--- directory/directory.py
+++ directory/directory.py
@@ -1,15 +1,16 @@
 from item.static import Item
 
-class Directory:
-    STRUCTURE = [
-        Item('inode', Item.TYPE_NUMERIC, 0, 4),
-        Item('rec_len', Item.TYPE_NUMERIC, 4, 2),
-        Item('name_len', Item.TYPE_NUMERIC, 6, 1),
-        Item('file_type', Item.TYPE_NUMERIC, 7, 1),
-        Item('name', Item.TYPE_BYTE, 8, 32),
-    ]
 
+class Directory:
     def __init__(self, block):
+        self.structure = [
+            Item('inode', Item.TYPE_NUMERIC, 0, 4),
+            Item('rec_len', Item.TYPE_NUMERIC, 4, 2),
+            Item('name_len', Item.TYPE_NUMERIC, 6, 1),
+            Item('file_type', Item.TYPE_NUMERIC, 7, 1),
+            Item('name', Item.TYPE_BYTE, 8, 32),
+        ]
+
         self.block = block
         self.inode = ''
         self.rec_len = ''
@@ -20,14 +21,8 @@ class Directory:
         self.read()
 
     def read(self):
-        for element in Directory.STRUCTURE:
+        for element in self.structure:
             setattr(self, element.name, element.get_value(self.block))
-    
+
     def __str__(self):
-        print(self.block)
-#        print(self.name)
-#        print(self.file_type)
-#        print(self.name_len)
-#        print(self.rec_len)
-#        print(self.inode)
         return ""
